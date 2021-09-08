f25267cf29d37d3d5e5ea4a1b15920ffff08c85a
learn13579 <maryana.poshta@gmail.com>
2021-09-08 18:34:03 +0300

    Stars

--- src/components/MoviesPage /MoviesPage.js
+++ src/components/MoviesPage /MoviesPage.js
@@ -2,6 +2,8 @@ import './MoviesPage.css';
 import {Link} from "react-router-dom";
 import img from "../MoviesListCard/5-Star.png";
 import {GenresIds} from "../GenreBadge/GenreBadge";
+// import StarRatings from "react-star-ratings/src";
+import ReactStars from "react-rating-stars-component/dist/react-stars";
 
 export default function MoviesPage(props) {
 
@@ -17,15 +19,18 @@ export default function MoviesPage(props) {
 
             <p>Movie rating: <b className={"rating"}>{state.vote_average}</b><img className={'star'} src={img}
                                                                                   alt="star"/></p>
-
             <p>Popularity: {state.popularity}</p>
 
             <p><b>Genres:</b> <br/><GenresIds genre_ids={state.genre_ids}/></p>
 
             <br/>
             <p><b>Overview:</b> <br/>{state.overview}</p>
+
+            <p> Please rate this movie: <ReactStars classNames={"reactStars"} count={state.vote_average} size={30}  /> </p>
+
             <br/>
             <p>release date: {state.release_date}</p>
+
             <p>original language: <b>{state.original_language}</b></p>
 
             <i>the number movie : {state.id}</i>
---
+++ src/components/StarsRating /StarsRating.js
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react'
+// import { Rating } from 'react-simple-star-rating'
+//
+// export function StarsRating() {
+//
+//     const [rating, setRating] = useState(0);
+//
+//     const handleRating = (rate) => {
+//         setRating(rate)
+//     }
+//
+//     return (
+//
+//         <div className='rating'>
+//             <Rating onClick={handleRating} ratingValue={rating}/>
+//         </div>
+//     )
+// }
